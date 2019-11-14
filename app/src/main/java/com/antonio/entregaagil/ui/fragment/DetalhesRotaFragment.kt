425bf1fe2a8573961a6163e16711dad7d14fcaad
Antonio <antonioneto_98@hotmail.com>
2019-11-13 22:22:02 -0300

    decimo primeiro commit

--- app/src/main/java/com/antonio/entregaagil/ui/fragment/DetalhesRotaFragment.kt
+++ app/src/main/java/com/antonio/entregaagil/ui/fragment/DetalhesRotaFragment.kt
@@ -7,23 +7,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.antonio.entregaagil.R
+import com.antonio.entregaagil.assinantes
 import com.antonio.entregaagil.constante.ASSINANTES_POR_ROTA
 import com.antonio.entregaagil.constante.ASSINANTE_TAG
 import com.antonio.entregaagil.constante.ROTA_TAG
 import com.antonio.entregaagil.modelo.Assinante
 import com.antonio.entregaagil.modelo.Rota
+import com.antonio.entregaagil.ui.adapter.list.AssinantesAdapter
 import com.antonio.entregaagil.ui.adapter.list.DetalhesRotaAdapter
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_detalhes_rota.*
+import kotlinx.android.synthetic.main.procurar_assinantes.view.*
 import org.koin.android.ext.android.inject
 
 class DetalhesRotaFragment : Fragment() {
 
+    private val adapterAssinates: AssinantesAdapter by inject()
     private val adapter: DetalhesRotaAdapter by inject()
     private var rota = Rota()
 
@@ -64,11 +68,23 @@ class DetalhesRotaFragment : Fragment() {
 
     private fun confguraFab() {
         fragment_detalhes_rota_fab.setOnClickListener {
-//            if (adapter.itemCount >= ASSINANTES_POR_ROTA) {
-//                Snackbar.make(fragment_detalhes_rota_fab, "O limite de cadastro por rota e de $ASSINANTES_POR_ROTA", Snackbar.LENGTH_SHORT).show()
-//            } else {
-                fragment_detalhes_rota_fab.findNavController().navigate(R.id.detalhes_rota_to_navigation_formulario_assinante)
-//            }
+            val builder = AlertDialog.Builder(context!!)
+            val view = layoutInflater.inflate(R.layout.procurar_assinantes, null, false)
+            view.procurar_assinantes_lista.adapter = adapterAssinates
+            val assinantes = assinantes()
+            adapterAssinates.atualizaLista(assinantes)
+            adapterAssinates.clickListener = { assinante, posicao ->
+                Toast.makeText(context, "asd", Toast.LENGTH_SHORT).show()
+            }
+
+            builder.setTitle("Selecione um assinante")
+            builder.setView(view)
+            builder.setPositiveButton("Cadastrar") { dialog, which ->
+
+            }
+            builder.setNegativeButton("Cancelar") { dialog, which -> dialog.cancel() }
+
+            builder.show()
         }
     }
 
@@ -79,7 +95,10 @@ class DetalhesRotaFragment : Fragment() {
         input.inputType = InputType.TYPE_CLASS_TEXT
         input.setText(rota.descricao)
 
-        val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val lp = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
         input.layoutParams = lp
         builder.setView(input)
         builder.setPositiveButton("Alterar") { dialog, which ->
